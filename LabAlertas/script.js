256d21defe49b89fc20940659c162b1535d96970
salobando <salobando2023@gmail.com>
2025-11-12 00:18:21 -0500

    Ejercicio1.Encuesta de Interes

--- LabAlertas/script.js
+++ LabAlertas/script.js
@@ -1,3 +1,4 @@
+document.getElementById("titulo").innerHTML = "Ejercicio 1. Encuesta de intereses"
 let votos = {
   web: 0,
   js: 0,
@@ -6,10 +7,31 @@ let votos = {
 };
 const form = document.getElementById("formulario-votos");
 
-function votos(){
-alert("Gracias por tu voto!!");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();//evita recargar la paginas
 
-}
+  const opcion = form.elements.opcion.value;
+  votos[opcion]++;
+  votos.total++;
+  for (let key in votos) {
+    const span = document.getElementById(`v${key}`);
+    if (span) {
+      span.textContent = votos[key];
+    }
+  }
+  alert("Gracias por tu voto!!");
+  form.reset(); // limpia la selección
 
+  if (votos.total % 5 === 0) {
+    console.log(`Total acumulado: ${votos.total}`);
+    document.getElementById("total").append(votos.total);
+  }
+})
 
+document.getElementById("titulo").innerHTML = "Ejercicio 2. Carrito de compras"
+let productos = [
+  {nombre: "manzana",precio: 1800},
+  {nombre: "sandia",precio: 3500},
+  {nombre: "arandanos",precio: 6800}
+];
 
